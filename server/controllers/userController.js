f698c24435e1a5075e3eb8eeb9d56f02cf7b1a94
Brusnicka <yerbamaniac@gmail.com>
2022-10-27 12:51:18 +0400

    Add user controller, device controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,30 @@
 const ApiError = require('../error/apiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign({id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
 
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
+        const {email, password, role} = req.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Incorrect email or password'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest('User with this email already created'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
 
     }
 
@@ -11,7 +34,7 @@ class UserController {
 
     async check(req, res, next) {
         const {id} = req.query
-        if(!id){
+        if (!id) {
             return next(ApiError.badRequest('id is missing'))
         }
         res.json(id)
